574d388e61e6165afcf0424d20c266628b6fcab6
aoi <aoi@d207-023-220-229.wireless.sfu.ca>
2023-03-03 14:53:23 -0800

    add jest for test

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {res.send("hello world")});
+
+//app.listen(port, () => {console.log(`example app listening on port ${port}`)});
+
+
+module.exports = app;
